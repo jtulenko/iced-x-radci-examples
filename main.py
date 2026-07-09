038037fdf0c2cc3718841121a0781da5cff6fc4f
Joe Tulenko <jtulenko@bgc.org>
2026-07-09 15:23:23 -0600

    updating templates

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def home():
     
     return render_template('home.html')
 
+
+@app.route('/icedexamples')
+def feedback():
+    
+    return render_template('icedexamples.html')
+
+
 @app.route('/radciexamples', methods=["GET", "POST"])
 def radciexamples():
 
@@ -33,86 +40,24 @@ def radciexamples():
 
     return render_template('radciexamples.html', script1=script1, div1=div1, rsl_site_list=rsl_site_list)
 
-@app.route('/sealevel', methods=["GET", "POST"])
-def sealevel():
-    script1,div1 = "",""
-    script2,div2 = "",""
-    if request.method == "POST":
-        action = request.form.get("action")
 
-        if action == "map_plot":
-            rsl_recon = str(request.form.get("rsl_recon"))
-            lat_min = int(request.form.get("lat_min", -90))
-            lat_max = int(request.form.get("lat_max", 90))
-            lon_min = int(request.form.get("lon_min", -180))
-            lon_max = int(request.form.get("lon_max", 180))
-            age_value = int(request.form.get("age_slider", 0))
-            rsl_max = int(request.form.get("rsl_max", 100))
-            rsl_min = int(request.form.get("rsl_min", -100))
-
-            script1, div1 = plotting.sealevel_plot1(lat_min, lat_max, lon_min, lon_max, age_value, rsl_recon, rsl_min, rsl_max)
-
-        elif action == "ts_plot":
-            ts_recon = str(request.form.get("rsl_reconTS"))
-            lat_index = int(request.form.get("lat_ts", 63))
-            lon_index = int(request.form.get("lon_ts", 19))
-            age_min = int(request.form.get("age_min", 0))
-            age_max = int(request.form.get("age_max", 400))
-
-            script2, div2 = plotting.sealevel_ts(lat_index, lon_index, age_min, age_max, ts_recon)
-
-
-    return render_template('sealevel.html', script1=script1, div1=div1, script2=script2, div2=div2)
-
-@app.route('/deformation')
-def deformation():
-    
-    return render_template('deformation.html')
-
-@app.route('/velocity', methods=["GET", "POST"])
-def velocity():
-    script1,div1 = "",""
-    if request.method == "POST":
-        action = request.form.get("action")
-
-        if action == "map_plot":
-            vdef_recon = str(request.form.get("vdef_recon"))
-            lat_min = int(request.form.get("lat_min", -90))
-            lat_max = int(request.form.get("lat_max", 90))
-            lon_min = int(request.form.get("lon_min", -180))
-            lon_max = int(request.form.get("lon_max", 180))
-            vdef_max = int(request.form.get("vdef_max", 10))
-            vdef_min = int(request.form.get("vdef_min", -10))
-
-            script1, div1 = plotting.vvel_plot1(lat_min, lat_max, lon_min, lon_max, vdef_recon, vdef_min, vdef_max)
-
-    
-    return render_template('velocity.html', script1=script1, div1=div1)
-
-@app.route('/gravity')
-def gravity():
-    
-    return render_template('gravity.html')
-
-@app.route('/stress')
-def stress():
-    
-    return render_template('stress.html')
-
-@app.route('/rotation')
-def rotation():
-    
-    return render_template('rotation.html')
-
-@app.route('/topography')
-def topography():
-    
-    return render_template('topography.html')
-
-@app.route('/feedback')
+@app.route('/icedxradci')
 def feedback():
     
-    return render_template('feedback.html')
+    return render_template('icedxradci.html')
+
+
+@app.route('/askiced')
+def feedback():
+    
+    return render_template('askiced.html')
+
+
+@app.route('/icedlab')
+def feedback():
+    
+    return render_template('icedlab.html')
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
