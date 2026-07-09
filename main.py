fe8193a1a38c92dc8c1fef7b4217a48acdf65126
Joe Tulenko <jtulenko@bgc.org>
2026-07-09 14:15:42 -0600

    this is dumb

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def radciexamples():
     
     query_result = dbconnect.querier_radci(rsl_site_query)
 
-    #rsl_site_list = query_result[1:,0].tolist()
-    rsl_site_list = query_result.ravel().tolist()[1:]
+    rsl_site_list = query_result[1:,0].astype(str).tolist()
 
     script1,div1 = "", ""
 
