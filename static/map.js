b0138f0cb7a12e1e414f9d12438bf1f21369eea7
Joe Tulenko <jtulenko@bgc.org>
2026-08-11 11:43:31 -0600

    ashrynryjrtjh

--- static/map.js
+++ static/map.js
@@ -1,7 +1,8 @@
 const map = new maplibregl.Map({
     container: 'map',
     style: 'https://demotiles.maplibre.org/style.json',
-    center: [0, 0],
+    center: [-90, 20],
     zoom: 1.5,
+    pitch: 30,
     projection: 'globe'
 });
