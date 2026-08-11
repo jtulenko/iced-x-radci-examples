39965a5a115aff5b134b3181cdd2322c2d485ed5
Joe Tulenko <jtulenko@bgc.org>
2026-08-11 11:54:30 -0600

    dgahwryjjketyi

--- static/map.js
+++ static/map.js
@@ -6,7 +6,32 @@ const map = new maplibregl.Map({
 });
 
 map.on('load', () => {
+
     map.setProjection({
         type: 'globe'
     });
+
+    fetch('https://version2.ice-d.org/api/leaflet_map/alpine?format=json')
+        .then(response => response.json())
+        .then(mapPayload => {
+
+            map.addSource('alpine-points', {
+                type: 'geojson',
+                data: mapPayload
+            });
+
+            map.addLayer({
+                id: 'alpine-points',
+                type: 'circle',
+                source: 'alpine-points',
+
+                paint: {
+                    'circle-radius': 5,
+                    'circle-color': '#ff0000',
+                    'circle-opacity': 0.8
+                }
+            });
+
+        });
+
 });
